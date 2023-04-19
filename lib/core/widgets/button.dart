7a087407367737d1f64a2be70f985b4d30c5ef5a
Nuibim Lewin <92654117+nlewin20@users.noreply.github.com>
2023-04-19 15:34:37 +0000

    updates

--- lib/core/widgets/button.dart
+++ lib/core/widgets/button.dart
@@ -11,6 +11,7 @@ class CustomButton extends StatelessWidget {
   final bool showIcon;
   final Color? bgColor;
   final Color? textColor;
+
   const CustomButton(
       {Key? key,
       required this.onPressed,
@@ -34,7 +35,7 @@ class CustomButton extends StatelessWidget {
           backgroundColor:
               isSecondary ? Colors.transparent : bgColor ?? theme.primaryColor,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(kDefaultPadding),
+            borderRadius: BorderRadius.circular(kDefaultPadding2x),
           ),
           elevation: 0),
       child: Center(
@@ -44,25 +45,90 @@ class CustomButton extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               showIcon
-                  ? SvgPicture.asset(
-                      iconLink!,
-                      color: isSecondary
-                          ? bgColor ?? theme.primaryColor
-                          : textColor ?? Colors.white,
+                  ? Padding(
+                      padding: const EdgeInsets.only(right: kDefaultPadding),
+                      child: SvgPicture.asset(
+                        iconLink!,
+                        color: isSecondary
+                            ? bgColor ?? theme.primaryColor
+                            : textColor ?? Colors.white,
+                      ),
                     )
-                  : const SizedBox(),
-              showIcon
-                  ? const SizedBox(width: kDefaultPadding)
                   : const SizedBox(),
               Text(
                 text,
-                style: theme.textTheme.bodyLarge!.copyWith(
+                style: theme.textTheme.bodyMedium!.copyWith(
                     color: isSecondary
                         ? bgColor ?? theme.primaryColor
                         : textColor ?? Colors.white),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButtonSmall extends StatelessWidget {
+  final VoidCallback onPressed;
+  final bool isSecondary;
+  final String text;
+  final String? iconLink;
+  final bool showIcon;
+  final Color? bgColor;
+  final Color? textColor;
+
+  const CustomButtonSmall(
+      {Key? key,
+      required this.onPressed,
+      required this.text,
+      this.iconLink,
+      this.showIcon = false,
+      this.isSecondary = false,
+      this.bgColor,
+      this.textColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+          side: isSecondary
+              ? BorderSide(color: bgColor ?? theme.primaryColor, width: 2)
+              : null,
+          backgroundColor:
+              isSecondary ? Colors.transparent : bgColor ?? theme.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(kDefaultPadding2x),
+          ),
+          elevation: 0),
+      child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            showIcon
+                ? Padding(
+                    padding: const EdgeInsets.only(right: kDefaultPadding / 2),
+                    child: SvgPicture.asset(
+                      iconLink!,
+                      color: isSecondary
+                          ? bgColor ?? theme.primaryColor
+                          : textColor ?? Colors.white,
+                    ),
+                  )
+                : const SizedBox(),
+            Text(
+              text,
+              style: theme.textTheme.bodyMedium!.copyWith(
+                  color: isSecondary
+                      ? bgColor ?? theme.primaryColor
+                      : textColor ?? Colors.white),
+            ),
+          ],
         ),
       ),
     );
