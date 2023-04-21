0ffd0c615f0adae7ee086b16b1e137b3cd96593d
Nuibim Lewin <92654117+nlewin20@users.noreply.github.com>
2023-04-21 21:57:37 +0000

    added routes config

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medlink/core/theme.dart';
+import 'package:medlink/screens/hospital_details_screen/hospital_details_screen.dart';
 
 import 'screens/homescreen/homescreen.dart';
 
@@ -18,8 +19,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: customLightTheme(context),
       home: const HomeScreen(),
+
+      /// Todo: rework routes to use Navigator 2.0
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+        HospitalDetailsScreen.id: (context) => HospitalDetailsScreen(),
+      },
     );
   }
 }
-
-
